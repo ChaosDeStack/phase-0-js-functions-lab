c45d2d43222bfb87401b947f19512d67e91a1478
Andy <mainawaweru018@gmail.com>
2026-09-25 22:02:45 +0300

    Added all the five functions and tested them

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
+function calculateTax(amount, taxRate) {
+    return amount * (taxRate / 100);
+}
 
+console.log(calculateTax(100, 10)); //Output: 10
 
+function convertToUpperCase(text) {
+    return text.toUpperCase(); 
+}
 
+console.log(convertToUpperCase("hello")); //Output: HELLO
+
+function findMaximum(num1, num2) {
+    return num1 > num2 ? num1:num2;
+}
+
+console.log(findMaximum(3, 7)); //Output: 7
+
+function isPalindrome(word) {
+    return word === word.split("").reverse().join("")
+}
+
+console.log(isPalindrome("level")); //Output: true
+console.log(isPalindrome("hello")); //Output: false
+
+function calculateDiscountedPrice(originalPrice, discountPrice) {
+    const discountAmount = originalPrice * (discountPrice / 100);
+    return originalPrice - discountAmount;
+}
+
+console.log(calculateDiscountedPrice(100, 20)); //Output: 80
 
 
 // This is required for the test to function properly  
-module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+//module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
