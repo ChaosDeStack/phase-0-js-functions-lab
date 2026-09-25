05d8eaa3135c9a7a8f5a8513aeb71639f6df5f72
Andy <mainawaweru018@gmail.com>
2026-09-25 22:17:43 +0300

    Fix module.exports-remove commented out slashes

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
+//function 1
 function calculateTax(amount, taxRate) {
     return amount * (taxRate / 100);
 }
 
 console.log(calculateTax(100, 10)); //Output: 10
 
+//function 2
 function convertToUpperCase(text) {
     return text.toUpperCase(); 
 }
 
 console.log(convertToUpperCase("hello")); //Output: HELLO
 
+//function 3
 function findMaximum(num1, num2) {
     return num1 > num2 ? num1:num2;
 }
 
 console.log(findMaximum(3, 7)); //Output: 7
 
+
+//function 4
 function isPalindrome(word) {
     return word === word.split("").reverse().join("")
 }
@@ -23,6 +28,7 @@ function isPalindrome(word) {
 console.log(isPalindrome("level")); //Output: true
 console.log(isPalindrome("hello")); //Output: false
 
+//function 5
 function calculateDiscountedPrice(originalPrice, discountPrice) {
     const discountAmount = originalPrice * (discountPrice / 100);
     return originalPrice - discountAmount;
@@ -32,4 +38,4 @@ console.log(calculateDiscountedPrice(100, 20)); //Output: 80
 
 
 // This is required for the test to function properly  
-//module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
